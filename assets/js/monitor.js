1fd5cb49b7e2d54e667a7327c0fc43a527bf0910
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-04-14 13:35:55 -0400

    feat: add visitor notifier

---
+++ assets/js/monitor.js
@@ -0,0 +1,23 @@
+(async function () {
+  const EVENT_NAME = 'website_visited';
+  const WEBHOOKS_KEY = 'cP7cTI1ZLRjZacB2QoWtvJ';
+  try {
+    const { ip } = await fetch('https://api.ipify.org?format=json').then(res => res.json());
+    const { href: url } = window.location;
+    const screenDimensions = `${window.screen.width} x ${window.screen.height}`;
+    const { referrer } = document;
+    const { userAgent } = navigator;
+    const strToSend = encodeURIComponent([
+      `IP: ${ip}`,
+      `URL: ${url}`,
+      `Screen Dimensions: ${screenDimensions}`,
+      `Referrer: ${referrer}`,
+      `User Agent: ${userAgent}`
+    ].join('<br>'));
+    await fetch(`https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${WEBHOOKS_KEY}?value1=${strToSend}`, {
+      method: 'GET'
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
