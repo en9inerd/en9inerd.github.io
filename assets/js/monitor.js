dcc37ae43fe28d9210715aca8b5eaad56a066286
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-04-15 01:17:34 -0400

    fix: recognize more

--- assets/js/monitor.js
+++ assets/js/monitor.js
@@ -2,13 +2,15 @@
   const EVENT_NAME = 'website_visited';
   const WEBHOOKS_KEY = 'cP7cTI1ZLRjZacB2QoWtvJ';
   try {
-    const { ip } = await fetch('https://cors-proxy.enginerd.workers.dev/').then(res => res.json());
+    const { ip, country, datacenter } = await fetch('https://cors-proxy.enginerd.workers.dev/').then(res => res.json());
     const { href: url } = window.location;
     const screenDimensions = `${window.screen.width} x ${window.screen.height}`;
     const { referrer } = document;
     const { userAgent } = navigator;
     const strToSend = encodeURIComponent([
       `IP: ${ip}`,
+      `Country: ${country}`,
+      `Datacenter: ${datacenter}`,
       `URL: ${url}`,
       `Screen Dimensions: ${screenDimensions}`,
       `Referrer: ${referrer}`,
