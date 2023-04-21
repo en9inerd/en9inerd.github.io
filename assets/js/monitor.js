061f4b648f88d303e93f99bf67f32d67f6bb8e88
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-04-21 13:29:03 -0400

    fix: update monitor.js script

--- assets/js/monitor.js
+++ assets/js/monitor.js
@@ -1,13 +1,14 @@
 (async function () {
   const EVENT_NAME = 'website_visited';
   const WEBHOOKS_KEY = 'cP7cTI1ZLRjZacB2QoWtvJ';
+  const CORS_PROXY = 'https://cors-proxy.enginerd.workers.dev?apiUrl=';
   try {
-    const { ip, country, datacenter } = await fetch('https://cors-proxy.enginerd.workers.dev/').then(res => res.json());
+    const { ip, country, datacenter } = await fetch(CORS_PROXY).then(res => res.json());
     const { href: url } = window.location;
     const screenDimensions = `${window.screen.width} x ${window.screen.height}`;
     const { referrer } = document;
     const { userAgent } = navigator;
-    const strToSend = encodeURIComponent([
+    const strToSend = [
       `IP: ${ip}`,
       `Country: ${country}`,
       `Datacenter: ${datacenter}`,
@@ -15,9 +16,11 @@
       `Screen Dimensions: ${screenDimensions}`,
       `Referrer: ${referrer}`,
       `User Agent: ${userAgent}`
-    ].join('<br>'));
-    await fetch(`https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${WEBHOOKS_KEY}?value1=${strToSend}`, {
-      method: 'GET'
+    ].join('<br>');
+    await fetch(CORS_PROXY + `https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${WEBHOOKS_KEY}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ value1: strToSend }),
     });
   } catch (error) {
     console.error(error);
