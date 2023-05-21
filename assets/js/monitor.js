d257d8c2eb77bd3b08d9973144247adde26aa3d7
Vladimir <18351400+en9inerd@users.noreply.github.com>
2023-05-21 16:03:24 -0400

    fix: update monitor.js

--- assets/js/monitor.js
+++ assets/js/monitor.js
@@ -1,26 +1,19 @@
 (async function () {
-  const EVENT_NAME = 'website_visited';
-  const WEBHOOKS_KEY = 'cP7cTI1ZLRjZacB2QoWtvJ';
-  const CORS_PROXY = 'https://cors-proxy.enginerd.workers.dev?apiUrl=';
+  const NOTIFIER = 'https://visitor-notifier.enginerd.workers.dev/';
   try {
-    const { ip, country, datacenter } = await fetch(CORS_PROXY).then(res => res.json());
     const { href: url } = window.location;
     const screenDimensions = `${window.screen.width} x ${window.screen.height}`;
     const { referrer } = document;
     const { userAgent } = navigator;
-    const strToSend = [
-      `IP: ${ip}`,
-      `Country: ${country}`,
-      `Datacenter: ${datacenter}`,
-      `URL: ${url}`,
-      `Screen Dimensions: ${screenDimensions}`,
-      `Referrer: ${referrer}`,
-      `User Agent: ${userAgent}`
-    ].join('<br>');
-    await fetch(CORS_PROXY + `https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${WEBHOOKS_KEY}`, {
+    await fetch(NOTIFIER, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ value1: strToSend }),
+      body: JSON.stringify({
+        URL: url,
+        'Screen Dimensions': screenDimensions,
+        Referrer: referrer,
+        'User Agent': userAgent,
+      }),
     });
   } catch (error) {
     console.error(error);
